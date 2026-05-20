5c3057e5be9805285bafd8c06030993638f96183
Никита <travin.nikita@gmail.com>
2026-05-20 23:03:54 +0300

    Roa 69 android user profiles (#18)
    * ROA-69: add get user profile api
    
    * ROA-69: add users basic profile screens, navigation from posts, comments, add transition animations

--- shared/src/commonMain/kotlin/dev/roasti/feature/auth/data/network/ProfileApiClient.kt
+++ shared/src/commonMain/kotlin/dev/roasti/feature/auth/data/network/ProfileApiClient.kt
@@ -1,20 +1,21 @@
 package dev.roasti.feature.auth.data.network
 
+import dev.roasti.core.network.ApiRoutes
+import dev.roasti.core.network.AuthorizedRequestExecutor
+import dev.roasti.feature.auth.data.network.model.request.UpdateProfileRequest
+import dev.roasti.feature.auth.data.network.model.response.PublicUserDto
+import dev.roasti.feature.auth.data.network.model.response.UserDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.patch
 import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
-import dev.roasti.core.network.ApiRoutes
-import dev.roasti.core.network.AuthorizedRequestExecutor
-import dev.roasti.feature.auth.data.network.model.request.UpdateProfileRequest
-import dev.roasti.feature.auth.data.network.model.response.UserDto
 
 interface ProfileApiClient {
     suspend fun getMyProfile(): Result<UserDto>
     suspend fun updateProfile(updateBody: UpdateProfileRequest): Result<UserDto>
+
+    suspend fun getUserProfile(username: String): Result<PublicUserDto>
 }
 
 class ProfileApiClientImpl(
@@ -23,14 +24,19 @@ class ProfileApiClientImpl(
 ) : ProfileApiClient {
 
     override suspend fun getMyProfile(): Result<UserDto> = authorizedRequestExecutor.execute { _ ->
-        httpClient.get(ApiRoutes.UsersMe).body<UserDto>()
+        httpClient.get(ApiRoutes.users.me).body<UserDto>()
     }
 
     override suspend fun updateProfile(updateBody: UpdateProfileRequest) =
         authorizedRequestExecutor.execute {
-            httpClient.patch(ApiRoutes.UsersMe) {
-                contentType(ContentType.Application.Json)
+            httpClient.patch(ApiRoutes.users.me) {
                 setBody(updateBody)
             }.body<UserDto>()
         }
+
+    override suspend fun getUserProfile(username: String): Result<PublicUserDto> {
+        return authorizedRequestExecutor.execute {
+            httpClient.get(ApiRoutes.users.byUsername(username)).body<PublicUserDto>()
+        }
+    }
 }
